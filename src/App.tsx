109be5d287a928181262752ac2f4d0051f8492b1
Tomás Mónaco <monacotomas99@gmail.com>
2023-09-20 13:47:42 +0200

    worked on structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Navbar } from "./components";
+
 const App: React.FC<NonNullable<unknown>> = () => {
-    return <div className="">Sos BB :)</div>;
+    return (
+        <div className="relative z-0 bg-primary">
+            <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+                <Navbar />
+            </div>
+        </div>
+    );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+// TODO: Move routes to separate file?
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
